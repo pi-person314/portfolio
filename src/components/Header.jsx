1268f7ef709f61962894fe3286077477aa82de6f
Jaden Li <jadenmengl@gmail.com>
2025-08-08 08:47:20 +0800

    header styling adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import moon from "../assets/images/moon.png";
 
 export default function Header({darkMode, toggleDarkMode}) {
     return (
-        <header className="flex justify-between space-x-5 bg-zinc-600 text-white p-6 font-montserrat">
+        <header className="flex justify-between space-x-4 bg-zinc-600 text-white px-4 py-6 font-montserrat">
             {/* home button (pi) */}
             <div className="flex items-center">
                 <Link to="/">
@@ -14,7 +14,7 @@ export default function Header({darkMode, toggleDarkMode}) {
             </div>
 
             {/* page links */}
-            <div className="flex justify-center items-center mx-auto space-x-10">
+            <div className="flex justify-center items-center mx-auto space-x-12 tiny:space-x-6">
                 <Link className="tiny:text-base text-2xl duration-300 hover:text-blue-300" to="/about">About</Link>
                 <Link className="tiny:text-base text-2xl duration-300 hover:text-blue-300" to="/projects">Projects</Link>
                 <Link className="tiny:text-base text-2xl duration-300 hover:text-blue-300" to="/contact">Contact</Link>
