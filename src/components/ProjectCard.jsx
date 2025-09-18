29abad134583938987091fbcded270ee3e0bb3b5
Jaden Li <jmli314@berkeley.edu>
2025-09-17 22:47:32 -0700

    adjusted project card for mobile

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,16 +4,16 @@ export default function ProjectCard({title, tech, desc, image, darkMode}) {
     const [ details, setDetails ] = useState(false);
 
     return (
-        <div className={`flex flex-col justify-between items-center shadow-xl rounded-xl space-y-8 p-10 ${darkMode ? "bg-zinc-700" : "bg-zinc-400"}`}>
+        <div className={`flex flex-col justify-between items-center shadow-xl rounded-xl space-y-8 p-8 tiny:px-6 ${darkMode ? "bg-zinc-700" : "bg-zinc-400"}`}>
             <h1 className="text-center text-2xl lg:text-3xl font-serif"><b>{title}</b></h1>
 
             {details ? 
-                <div className="flex flex-col justify-center space-y-4 h-52">
+                <div className="flex flex-col justify-center space-y-4 h-60">
                     <p className="text-center text-sm lg:text-base italic">{tech}</p>
                     <ul className="text-sm lg:text-base list-disc list-inside">{desc.map(bullet => <li>{bullet}</li>)}</ul>
                 </div>
             : 
-                <img className="h-52 object-contain" src={image} alt={title}/>
+                <img className="h-60 object-contain" src={image} alt={title}/>
             }
 
             <p onClick={() => setDetails(!details)} className={`text-sm lg:text-base underline duration-300 cursor-pointer ${darkMode ? "hover:text-zinc-400" : "hover:text-zinc-500"}`}>{details ? "Hide" : "View"} Details</p>
--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -5,7 +5,7 @@ import ProjectCard from "../components/ProjectCard.jsx";
 
 export default function Projects({darkMode}) {
     return (
-        <div className="tiny:flex tiny:flex-col grid grid-cols-2 lg:auto-rows-[28rem] gap-10 p-10 overflow-y-auto h-full font-montserrat">
+        <div className="tiny:flex tiny:flex-col grid grid-cols-2 lg:auto-rows-[28rem] gap-10 p-8 overflow-y-auto h-full font-montserrat">
             <ProjectCard 
                 title={<a href="https://drill-share.vercel.app" target="_blank" className={`underline duration-300 ${darkMode ? "hover:text-blue-300" : "hover:text-blue-600"}`}>
                     Drill Share
